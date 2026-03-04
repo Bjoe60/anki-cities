cfe25e7bb70dfff189d22ff9d5b86721685bae6a
Bjørn <bjorn60@gmail.com>
2026-03-04 11:48:15 +0100

    Remove old files

--- tests/City count.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-pd.set_option('display.max_rows', None)
-
-df = pd.read_csv('Cities of countries.csv', dtype='unicode')
-print(df['actual_country'].value_counts())
--- tests/duplicates.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-duplicate_check = 'wikipedia_id'
-df = pd.read_csv('dbpedia.csv', dtype='unicode')
-df = df[df.duplicated(subset=[duplicate_check], keep=False)].drop_duplicates(subset=[duplicate_check])
-df = df.sort_values(duplicate_check)
-for i in df[duplicate_check]:
-    print(i)
